9912baada0809cb2bf5848e7d856d940d1f6dc1a
ACER\HI <kanneganti.joshika@gmail.com>
2018-03-14 23:48:38 +0530

    main program basics

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,4 +7,14 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+	public int mul(int a, int b){
+		return a*b;
+	}
+	public int div(int a, int b){
+		return a/b;
+	}
+	
 }
